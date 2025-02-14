04475a89fbd1550c1d333165e89caa6e3c02996e
suman <sumansapkota777@gmail.com>
2025-02-14 10:04:23 +0545

    feat:multer

--- routes/groundRoute/groundRoute.js
+++ routes/groundRoute/groundRoute.js
@@ -3,12 +3,19 @@ const getGround = require("../../controller/ground/getGround");
 const createGround = require("../../controller/ground/createGround");
 const isAuthenicated = require("../../middleware/isAuthenticated");
 const restrictTo = require("../../middleware/restrictTo");
+const { multer, storage } = require("../../middleware/multerConfig");
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
 router.route("/getground").get(isAuthenicated, getGround);
 router
   .route("/createground")
-  .post(isAuthenicated, restrictTo("admin"), createGround);
+  .post(
+    isAuthenicated,
+    restrictTo("admin"),
+    upload.single(groundImage),
+    createGround
+  );
 
 module.exports = router;
