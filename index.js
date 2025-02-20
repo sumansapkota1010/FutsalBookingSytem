17ae345f14490388355282e32370b0b39e987ee9
suman <sumansapkota777@gmail.com>
2025-02-20 22:38:36 +0545

    fixed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const connectDB = require("./database/database");
-const User = require("./models/userModel");
+const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.static("uploads"));
 
